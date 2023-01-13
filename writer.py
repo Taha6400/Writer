44c4b88aa10ba0a7ae881d71b12d0f6798cb9af7
Taha6400 <113461712+Taha6400@users.noreply.github.com>
2023-01-13 16:04:02 +0330

    added writer
    adds the writer

--- writer.py
+++ writer.py
@@ -1,3 +1,7 @@
 about = input("Please type something to write an Essay About!\n")
 
-print(f"{about} is a Pretty good thing! you can do all sorts of thing with it. {about} will help you on anything!")
+print(about,"is a Pretty good thing! you can do all sorts of thing with it.")
+print(about,"can help you in anything!")
+print("with", about,"you can do anything, even hack people.")
+print("if you lost your",about,", its okay You can get another one!")
+print("End of the Essay")
