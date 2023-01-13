a31ebd084b0163dd48fd52027afd33c186e68431
Taha6400 <113461712+Taha6400@users.noreply.github.com>
2023-01-13 15:50:23 +0330

    added writer

---
+++ writer.py
@@ -0,0 +1,3 @@
+about = input("Please type something to write an Essay About!\n")
+
+print(f"{about} is a Pretty good thing! you can do all sorts of thing with it. {about} will help you on anything!")
